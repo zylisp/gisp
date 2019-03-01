ab617dfb40677a216265456377288fbe0367e049
Duncan McGreggor <oubiwann@gmail.com>
2019-03-01 06:12:43 -0600

    Benchmark was calling wrong function.

--- play/func_call_benchmark_test.go
+++ play/func_call_benchmark_test.go
@@ -10,7 +10,7 @@ func BenchmarkFuncCallSpeedWithInner(b *testing.B) {
 
 func BenchmarkFuncCallSpeedWithoutInner(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		withInner()
+		withoutInner()
 	}
 }
 
