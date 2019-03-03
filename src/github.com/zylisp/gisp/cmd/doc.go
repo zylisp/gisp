07c4097f181d3ed816be84d836b0bff4744bbc66
Duncan McGreggor <oubiwann@gmail.com>
2019-03-03 10:33:51 -0600

    Removed unused doc file.

--- src/github.com/zylisp/gisp/cmd/doc.go
+++
@@ -1,7 +0,0 @@
-/*
-
-The gisp/cmd package has no code of it's own; please see the subdirectories
-for docs on supported commands.
-
-*/
-package cmd
--- src/github.com/zylisp/gisp/gitcommit.go
+++ src/github.com/zylisp/gisp/gitcommit.go
@@ -1,3 +1,3 @@
 package gisp
 
-func init() { GITLASTTAG = "0.7.0-alpha1"; 	GITLASTCOMMIT = "5bfb54c" }
+func init() { GITLASTTAG = "0.7.0-alpha1"; 	GITLASTCOMMIT = "29ed02a" }
