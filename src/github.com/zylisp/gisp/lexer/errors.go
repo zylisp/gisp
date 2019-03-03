a9388feeb3f5948beb4be9e665165748b7f20006
Duncan McGreggor <oubiwann@gmail.com>
2019-03-03 10:25:44 -0600

    Separated lexer errors into separate file.

---
+++ src/github.com/zylisp/gisp/lexer/errors.go
@@ -0,0 +1,5 @@
+package lexer
+
+const UnsupportedRuneError string = "Don't know what to do with rune: %q"
+const UnterminatedQuotedStringError string = "Unterminated quoted string"
+const BadNumberSyntax string = "Bad number syntax: %q"
